b643d4a2b917a55279d0d6784948fd879a45a4e4
Saish Gersappa <sgersapp@cisco.com>
2017-07-06 11:57:06 -0700

    sgersapp: dynamic version
    Change-Id: I74d0c8fc1eb966e714f4e1ccad67267e1c10b2ce

--- dynamic/max_ascending_subsequence.py
+++ dynamic/max_ascending_subsequence.py
@@ -1,28 +1,45 @@
 
 
-SEQ = (1, 2, 3, 2, 6, 8, 9, 15, 22, 2, 3, 4, 5, 6, 11, 4, 7, 29, 5, 6, 7, 8, 9)
+SEQ = (1, 2, 3, 8, 9, 15, 22, 2, 3, 4, 11, 4, 7, 29, 40, 45, 90, 8, 9, 15, 22, 2, 3, 4, 7, 7, 29, 40, 45, 90)
+#SEQ = (1, 2, 1, 3, 100, 2, 2, 3, 2, 100, 2, 3, 1, 2)
+#SEQ = (1, 2, 3, 2, 6,)
 #SEQ = (1, 6, 4)
+#SEQ = (1, 6, 4, 5, 2, 3, 4)
 
 def is_ascending(subseq):
     prev = -1
     for i in subseq:
-        if prev > i:
+        if prev >= i:
             return False
         else:
            prev = i
     return True
-            
 
 def recurse(subseq=(), index=0):
     if index == len(SEQ):
-        if is_ascending(subseq):
-            return len(subseq)
-        else:
-            return 0
+        return len(subseq)
+    else:
+        value1 = recurse(subseq, index+1)
 
-    return max(recurse(subseq, index+1),
-               recurse(subseq + (SEQ[index],), index+1))
+        new_subseq = subseq + (SEQ[index],)
+        value2 = 0
+        if is_ascending(new_subseq):
+            value2 = recurse(new_subseq, index+1)
+
+        return max(value1, value2)
+
+
+
+def dynamic():
+    CACHE2 = [1] * len(SEQ)
+
+    for i in xrange(len(SEQ)):
+        for j in xrange(i+1, len(SEQ)):
+            if SEQ[j] > SEQ[i]:
+                CACHE2[j] = max(CACHE2[i] + 1, CACHE2[j])
+    return max(CACHE2)
 
 
 if __name__ == '__main__':
     print recurse()
+    print dynamic()
